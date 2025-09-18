6f7545c97c0b3ee71a5aba097c783ca3eae14541
s18914 <emilka.cholewicka@gmail.com>
2025-09-18 10:39:33 +0200

    add rn-router

---
+++ app/index.tsx
@@ -0,0 +1,20 @@
+import { View, Text, StyleSheet } from "react-native";
+
+const Home = () => (
+  <View style={styles.container}>
+    <Text style={styles.text}>Welcome to the Home Screen!</Text>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 20,
+  },
+});
+
+export default Home;
